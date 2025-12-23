e9b770b621d305bacf9a46929f513284388d6f94
controlwork <rasulkimsanbaevich@gmail.com>
2025-12-23 23:35:04 +0600

    control

--- oop.py
+++ oop.py
@@ -1,20 +1,20 @@
 class Person:
-    def __init__(self, age):
-        self._age = age
+    def __init__(self):
+        self._age = 0
 
     def set_age(self, age):
-        # if age == self._age:
-        #     return self._age
-            pass
-
+        if age < 0:
+            print("Ошибка: возраст не может быть отрицательным")
+        else:
+            self._age = age
 
     def get_age(self):
         return self._age
 
-# p = Person()
-# p.set_age(25)
-# print(p.get_age())  # Вывод: 25
-# p.set_age(-5)  # Должна быть ошибка или предупреждение
+p = Person()
+p.set_age(25)
+print(p.get_age())  # Вывод: 25
+p.set_age(-5)  # Должна быть ошибка или предупреждение
 
 
 # 2
@@ -43,9 +43,53 @@ print(cat.name, cat.speak())  # Вывод: Kitty Meo
 
 class Vehicle:
     def move(self):
-        ...
-class Car:
-    pass
+        return "Vehicle is moving"
+class Car(Vehicle):
+    def move(self):
+        return "Car is driving"
 
-class Bike:
-    pass
+class Bike(Vehicle):
+    def move(self):
+        return "Bicycle is pedaling"
+
+def move(vehicle):
+    return vehicle.move()
+
+car = Car()
+bike = Bike()
+
+print(move(car))  # Вывод: Car is driving
+print(move(bike))  # Вывод: Bicycle is pedaling
+
+#4
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.1416 * self.radius * self.radius
+
+
+rect = Rectangle(10, 5)
+circle = Circle(7)
+
+print(rect.area())     # 50
+print(round(circle.area(), 2))  # 153.94
